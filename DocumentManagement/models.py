133497f0dfb38f7023d9252c60e70cb2f8a6abdb
Cecily Devaney <tul06320@temple.edu>
2022-03-24 14:02:55 -0400

    Pushing recent updates - file split not actively working right now

--- DocumentManagement/models.py
+++ DocumentManagement/models.py
@@ -1,7 +1,8 @@
 from django.conf import settings
 from django.db import models
 from backend.storage_backends import MediaStorage
-
+from pdf2image import convert_from_path, convert_from_bytes
+import io
 
 # Create your models here.
 
@@ -11,11 +12,6 @@ from backend.storage_backends import MediaStorage
 # Deck List
 
 class Document(models.Model):
-    FILE_EXT = [
-        'jpg',
-        'png',
-        'pdf'
-    ]
     MODE_CHOICES = [
         ('TRL', 'Translation'),
         ('DEF', 'Definition')
@@ -31,8 +27,32 @@ class Document(models.Model):
 
     #saves file to user folder, and filename becomes folder for all files associated with document
     def save(self, *args, **kwargs):
-        self.file.name = (str(self.owner_id) + "/" + self.filename + "/" + self.file.name)
-        super(Document, self).save(*args, **kwargs)
+        orig_name = self.file.name
+        if (self.file.name[-3:]=='pdf'):
+
+            ###Does not work proprely right now
+            images = convert_from_bytes(self.file.read(), poppler_path=r'C:\Program Files\poppler-0.68.0\bin', fmt="png")
+            for i, image in enumerate(images):
+                fname=orig_name[:-4] + str(i) + ".png"
+                print(fname)
+                print(image)
+                #save file locally
+                #image.save(fname)
+
+                ##trying to store file as in variable rather than local filesystem
+                buffer = io.BytesIO()
+                #image.save(buffer, "PNG")
+                #self.file.save(fname, image)
+
+                ##issues trying to figure out how to save a local var or update
+                doc = Document(owner_id=self.owner_id, filename=self.file.name, file=buffer,
+                               mode=self.mode, language=self.language,
+                               trans_language=self.trans_language)
+                self.file.name = (str(self.owner_id) + "/" + self.filename + "/" + self.file.name)
+                doc.save()
+        else:
+            self.file.name = (str(self.owner_id) + "/" + self.filename + "/" + self.file.name)
+            super(Document, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.filename
