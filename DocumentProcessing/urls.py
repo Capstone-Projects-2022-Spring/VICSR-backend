5d5f6fc8a076621d7a5bc37acc6c25b17a1dded6
Cecily Devaney <tul06320@temple.edu>
2022-04-09 16:15:12 -0400

    Can update file with highlight data, and then return the associated document's study set

--- DocumentProcessing/urls.py
+++ DocumentProcessing/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', FileView.ApiOverview, name='files'),
     path('add/', FileView.add_file, name='add_file'),
     path('list/', FileView.get_files, name='get_files'),
-    path('delete/<int:pk>', FileView.delete_file, name='delete_file')
+    path('delete/<int:pk>', FileView.delete_file, name='delete_file'),
+    path('update/<int:pk>', FileView.update_highlight, name='update_highlight')
 ]
