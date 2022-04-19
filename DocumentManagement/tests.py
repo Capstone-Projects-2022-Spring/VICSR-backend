0a8f7569b77c2cadd5be46576c4c836f4390d54d
Cecily Devaney <tul06320@temple.edu>
2022-04-18 20:10:22 -0400

    Finished doc management tests

--- DocumentManagement/tests.py
+++ DocumentManagement/tests.py
@@ -2,7 +2,6 @@ from django.test import TestCase, Client
 from rest_framework.test import APIClient
 from DocumentManagement.models import Document
 from AccountManagement.models import User
-from rest_framework.authtoken.models import Token
 
 
 # Create your tests here.
@@ -14,20 +13,9 @@ class DocumentTestCase(TestCase):
                                 trans_language= 'fr')
         Document.objects.create(owner_id=self.user, filename='doc2', mode='DEF', language='en')
         self.client.login(email='test@test.com', password='testpass1')
-
-
-    def test_getDocs(self):
-        docs = Document.objects.filter(owner_id=self.user)
-        self.assertEqual(docs.count(), 2)
-
-    def test_addDoc(self):
-        doc = Document.objects.create(owner_id=self.user, filename='test_doc', mode= 'TRL', language= 'en', trans_language= 'fr')
-        self.assertEqual(doc.filename, 'test_doc')
-        docs = Document.objects.filter(owner_id=self.user)
-        self.assertEqual(docs.count(), 3)
+        self.client.force_authenticate(user=self.user)
 
     def test_addDocAPI(self):
-        self.client.force_authenticate(user=self.user)
         with open('DocumentManagement/testDocuments/highlightDEskewed.png', 'rb') as fp:
             response = self.client.post('/api/docs/add/', {'filename': 'test_doc', 'file': fp, 'mode': 'TRL',
                                                   'language': 'en', 'trans_language': 'fr'})
@@ -36,7 +24,23 @@ class DocumentTestCase(TestCase):
         self.assertEqual(docs.count(), 3)
 
     def test_getDocsAPI(self):
-        self.client.force_authenticate(user=self.user)
         response = self.client.get('/api/docs/list/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.data), 2)
+
+    def test_deleteDocAPI(self):
+        doc = Document.objects.filter(owner_id=self.user).first()
+        url = '/api/docs/delete/' + str(doc.id)
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, 202)
+        docs = Document.objects.filter(owner_id=self.user)
+        self.assertEqual(len(docs), 1)
+
+    def test_renameDocAPI(self):
+        doc = Document.objects.filter(owner_id=self.user).first()
+        url = '/api/docs/update/' + str(doc.id)
+        response = self.client.post(url, {'filename': 'NEW_NAME'})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, 'NEW_NAME')
+
+
